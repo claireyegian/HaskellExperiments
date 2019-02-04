7c2d3b1d1bd03a4a1b3590de5114e0982cdc3a77
clYeaigi21 Hhs <clyeaigi21@hanovernorwichschools.org>
2019-02-04 15:23:55 +0000

    Initial commit

--- PTfunctions.hs
+++ PTfunctions.hs
@@ -2,11 +2,11 @@ module PTfunctions where
 
 quadraticVertex a b c = (0-b)/(2*a)
 quadraticDiscriminant a b c = (b**2)-(4*a*c)
-quatraticRealSolutionQty a b c = if (quadraticDiscriminant a b c)<=0 
+quadraticRealSolutionQty a b c = if (quadraticDiscriminant a b c)<=0 
     then if (quadraticDiscriminant a b c)==0 
-        then putStrLn "1"
-        else putStrLn "0"
-else putStrLn "2"
+        then 1
+        else 0
+else 2
 
 convertFtoC f = (f-32)/1.8
 convertCtoF c = (1.8*c)+32
